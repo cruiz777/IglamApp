2f3774eae4f551565a34072f1fc028f9d399391d
cruiz777 <cristian_ruiz_777@hotmail.com>
2022-05-13 18:09:14 -0500

    Agregar archivos de proyecto.

---
+++ IglamApp/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace IglamApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Login_Clicked(object sender, EventArgs e)
+        {
+            string usuario =txtUsuario.Text;
+            string clave=txtClave.Text;
+            if(usuario=="admin" && clave=="admin")
+            {
+                DisplayAlert("Advertencia", "Bienvenido: "+usuario, "Ok");
+                await Navigation.PushAsync(new Principal());
+
+            }
+            else
+            {
+                _ = DisplayAlert("Advertencia", "Datos Incorrectos", "Ok");
+                txtUsuario.Text = "";
+                txtClave.Text = "";
+
+            }
+        }
+    }
+}
